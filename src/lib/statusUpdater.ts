029f69f2e2e00a005fa03fd8beb33cea459cbbb1
Andrea Calici <andrea.calici@mia-platform.eu>
2023-03-27 10:44:56 +0200

    Cleanup code

---
+++ src/lib/statusUpdater.ts
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2022 Mia srl
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ConsumerState, ProducerState } from './types'
+
+export class KafkaJSStatusUpdater {
+  // consumer
+  setConsumerConnectStatus(consumerState: ConsumerState): void {
+    consumerState.status = { healthy: true, ready: false }
+  }
+  setConsumerGroupJoinStatus(consumerState: ConsumerState): void {
+    consumerState.status = { healthy: true, ready: true }
+  }
+  setConsumerStopStatus(consumerState: ConsumerState): void {
+    consumerState.status = { healthy: true, ready: false }
+  }
+  setConsumerDisconnectStatus(consumerState: ConsumerState): void {
+    consumerState.status = { healthy: false, ready: false }
+  }
+  setConsumerCrashStatus(consumerState: ConsumerState, event: any): void {
+    consumerState.status = { healthy: event.payload.restart, ready: false }
+  }
+
+  // producer
+  setProducerConnectStatus(producerState: ProducerState): void {
+    producerState.status = { healthy: true, ready: true }
+  }
+  setProducerDisconnectStatus(producerState: ProducerState): void {
+    producerState.status = { healthy: false, ready: false }
+  }
+}
